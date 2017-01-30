3b36ac27d5ef8553ea0e2c5fd8d6139dafef4860
fiveseven808 <yosh.five7@gmail.com>
2017-01-30 13:51:53 -1000

    170130: 1349 hueberry.py Update
    UNTESTED.
    Display = 5 will now play AND record scene 1.
    Hold down for 5 seconds on display 5 to record a scene.
    Push display 5 for less than 5 seconds to play back scene 1.
    
    None of this is tested yet.

--- hueberry.py
+++ hueberry.py
@@ -1338,7 +1338,7 @@ while True:
         elif(display == 4):
             display_2lines(str(display) + ". Turn OFF","all lights quickly",17)
         elif(display == 5):
-            display_2lines(str(display) + ". Record by Light","Scene 1",13)
+            display_2lines(str(display) + ". Whole House","Scene 1",13)
         elif(display == 6):
             display_2lines(str(display) + ". 5am","Wakeup",17)
         elif(display == 7):
@@ -1408,16 +1408,21 @@ while True:
             hue_groups(lnum = "0",lon = "false",lbri = "256",lsat = "256",lx = "-1",ly = "-1",ltt = "4",lct = "-1")
             debugmsg("turning all lights off quick")
         elif(display == 5):
-            display_2lines("Turning lights:","Scene 1",12)
-            os.popen("./1_scene.sh")
-            time.sleep(1)
-            debugmsg("turning lights scene1")
-            
-            scenenumber = 1
-            ltt = 100
-            #result = get_house_scene_by_group(scenenumber,ltt)
-            result = get_house_scene_by_light(scenenumber,ltt)
-            debugmsg("ran scene by group creation with result = " + result)
+            result = holding_button(5000,"Hold to edit S1","Will record S1",21)
+            if result == 0:
+                display_2lines("Turning lights:","Scene 1",12)
+                os.popen("./1_scene.sh")
+                time.sleep(1)
+                debugmsg("turning lights scene1")
+            elif result == 1:
+                scenenumber = 1
+                ltt = 100
+                #result = get_house_scene_by_group(scenenumber,ltt)
+                result = get_house_scene_by_light(scenenumber,ltt)
+                debugmsg("ran scene by group creation with result = " + result)
+            else:
+                display_2lines("Something weird","Happened...",12)
+                time.sleep(5)
         elif(display == 6):
             display_2lines("Turning lights:","5am wakeup",12)
             os.popen("./5amwakeup_scene.sh")
